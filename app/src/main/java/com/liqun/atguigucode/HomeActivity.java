61bcd97062bb294f7c888d535382a72137131639
MoeRookie <MoeRookie@163.com>
2019-04-02 20:20:11 +0800

    设置默认选中第一个Tab

--- app/src/main/java/com/liqun/atguigucode/HomeActivity.java
+++ app/src/main/java/com/liqun/atguigucode/HomeActivity.java
@@ -3,11 +3,20 @@ package com.liqun.atguigucode;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.widget.RadioGroup;
 
 public class HomeActivity extends FragmentActivity {
+
+    private RadioGroup mRgHome;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        /**
+         * 默认选中第一个Tab
+         */
+        mRgHome = findViewById(R.id.rg_home);
+        mRgHome.check(R.id.rb_common_frame);
     }
 }
