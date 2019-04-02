bdc9cbb1f84f872b23aae049eac596622d516502
MoeRookie <MoeRookie@163.com>
2019-04-02 13:21:22 +0800

    1.软件框架搭建

--- app/src/main/java/com/liqun/atguigucode/SplashActivity.java
+++ app/src/main/java/com/liqun/atguigucode/SplashActivity.java
@@ -3,6 +3,9 @@ package com.liqun.atguigucode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 1、软件框架搭建
+ */
 public class SplashActivity extends AppCompatActivity {
 
     @Override
