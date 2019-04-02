605479dd4c16c0776daf263faf81dcb33b108e4d
MoeRookie <MoeRookie@163.com>
2019-04-02 18:51:22 +0800

    延时跳转主界面

---
+++ app/src/main/java/com/liqun/atguigucode/HomeActivity.java
@@ -0,0 +1,12 @@
+package com.liqun.atguigucode;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+public class HomeActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/liqun/atguigucode/SplashActivity.java
+++ app/src/main/java/com/liqun/atguigucode/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.liqun.atguigucode;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +14,29 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        /**
+         * 延时跳转主界面
+         */
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /**
+                 * 在主线程中执行(postDelayed()方法在主线程中被调用)
+                 * -> 跳转主界面
+                 */
+                enterHome();
+
+            }
+        },2000);
+    }
+
+    /**
+     * 跳转主界面
+     */
+    private void enterHome() {
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
