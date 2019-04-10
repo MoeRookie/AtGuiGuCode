4ea4c3fba21d35f227863fc88f189e9ded99f678
MoeRookie <MoeRookie@163.com>
2019-04-10 18:40:21 +0800

    原生OkHttp联网请求文本数据(post)并使用handler更新请求结果为文本内容

--- app/src/main/java/com/liqun/atguigucode/activity/OkHttpActivity.java
+++ app/src/main/java/com/liqun/atguigucode/activity/OkHttpActivity.java
@@ -13,15 +13,20 @@ import com.liqun.atguigucode.R;
 
 import java.io.IOException;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OkHttpActivity extends Activity
 implements View.OnClickListener {
     private static final String TAG = "OkHttpActivity";
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
     public static final int GET = 1;
-    private Button mBtnGet;
+    public static final int POST = 2;
+    private Button mBtnGet,mBtnPost;
     private TextView mTvResult;
     private OkHttpClient mClient = new OkHttpClient();
     private Handler mHandler = new Handler(){
@@ -30,6 +35,10 @@ implements View.OnClickListener {
             super.handleMessage(msg);
             switch (msg.what) {
                 case GET:
+                    // 将result设置给mTvResult以显示
+                    mTvResult.setText((String) msg.obj);
+                    break;
+                case POST:
                     // 将result设置给mTvResult以显示
                     mTvResult.setText((String) msg.obj);
                     break;
@@ -49,11 +58,13 @@ implements View.OnClickListener {
 
     private void initView() {
         mBtnGet = findViewById(R.id.btn_get);
+        mBtnPost = findViewById(R.id.btn_post);
         mTvResult = findViewById(R.id.tv_result);
     }
 
     private void setListener() {
         mBtnGet.setOnClickListener(this);
+        mBtnPost.setOnClickListener(this);
     }
 
     @Override
@@ -62,6 +73,11 @@ implements View.OnClickListener {
             case R.id.btn_get:
                 // 原生OkHttp联网get请求文本数据
                 getDataByGet();
+                break;
+            case R.id.btn_post:
+                // 原生OkHttp联网post请求文本数据
+                mTvResult.setText("");
+                getDataByPost();
                 break;
             default:
                 break;
@@ -92,6 +108,37 @@ implements View.OnClickListener {
     private String get(String url) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
+                .build();
+        Response response = mClient.newCall(request).execute();
+        return response.body().string();
+    }
+
+    /**
+     * 原生OkHttp联网post请求文本数据
+     */
+    private void getDataByPost() {
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    String result = post("http://api.m.mtime.cn/PageSubArea/TrailerList.api","");
+                    Log.e(TAG, "result = " + result);
+                    Message message = Message.obtain();
+                    message.what = POST;
+                    message.obj = result;
+                    mHandler.sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    private String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
                 .build();
         Response response = mClient.newCall(request).execute();
         return response.body().string();
