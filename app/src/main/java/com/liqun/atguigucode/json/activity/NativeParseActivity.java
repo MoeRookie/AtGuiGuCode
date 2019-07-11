1f9ab5e03924c82a2b83c6aef8abed359a5b4400
MoeRookie <MoeRookie@163.com>
2019-07-11 19:25:00 +0800

    初始化view和button点击监听

--- app/src/main/java/com/liqun/atguigucode/json/activity/NativeParseActivity.java
+++ app/src/main/java/com/liqun/atguigucode/json/activity/NativeParseActivity.java
@@ -3,6 +3,9 @@ package com.liqun.atguigucode.json.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +18,16 @@ import com.liqun.atguigucode.R;
  * （3）复杂json数据解析
  * （4）特殊json数据解析
  */
-public class NativeParseActivity extends AppCompatActivity {
+public class NativeParseActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTvTitle;
+    private Button mBtnJsonObj;
+    private Button mBtnArrList;
+    private Button mBtnComplex;
+    private Button mBtnSpecial;
+    private TextView mTvOriginal;
+    private TextView mTvTransformed;
+
     public static Intent newIntent(Context packageContext) {
         Intent intent = new Intent(packageContext, NativeParseActivity.class);
         return intent;
@@ -25,5 +37,43 @@ public class NativeParseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_native_parse);
+        initView();
+        setListener();
+    }
+
+    private void setListener() {
+        mBtnJsonObj.setOnClickListener(this);
+        mBtnArrList.setOnClickListener(this);
+        mBtnComplex.setOnClickListener(this);
+        mBtnSpecial.setOnClickListener(this);
+    }
+
+    private void initView() {
+        mTvTitle = findViewById(R.id.tv_title);
+        mTvTitle.setText("手动Json解析");
+        mBtnJsonObj = findViewById(R.id.btn_json_obj);
+        mBtnArrList = findViewById(R.id.btn_arr_list);
+        mBtnComplex = findViewById(R.id.btn_complex);
+        mBtnSpecial = findViewById(R.id.btn_special);
+        mTvOriginal = findViewById(R.id.tv_original);
+        mTvTransformed = findViewById(R.id.tv_transformed);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            // 将json格式的字符串{}转换为Java对象
+            case R.id.btn_json_obj:
+                break;
+            // 将json格式的字符串[]转换为Java对象的List
+            case R.id.btn_arr_list:
+                break;
+            // 复杂json数据解析
+            case R.id.btn_complex:
+                break;
+            // 特殊json数据解析
+            case R.id.btn_special:
+                break;
+        }
     }
 }
