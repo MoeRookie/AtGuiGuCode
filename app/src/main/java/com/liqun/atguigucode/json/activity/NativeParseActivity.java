4276e103eebabe929da6b67e450721704ab0d9e0
MoeRookie <MoeRookie@163.com>
2019-07-11 20:07:23 +0800

    将json格式的字符串{}转换为Java对象

--- app/src/main/java/com/liqun/atguigucode/json/activity/NativeParseActivity.java
+++ app/src/main/java/com/liqun/atguigucode/json/activity/NativeParseActivity.java
@@ -3,6 +3,7 @@ package com.liqun.atguigucode.json.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.liqun.atguigucode.R;
+import com.liqun.atguigucode.json.bean.ShopBean;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * （1）将json格式的字符串{}转换为Java对象
@@ -19,7 +24,6 @@ import com.liqun.atguigucode.R;
  * （4）特殊json数据解析
  */
 public class NativeParseActivity extends AppCompatActivity implements View.OnClickListener {
-
     private TextView mTvTitle;
     private Button mBtnJsonObj;
     private Button mBtnArrList;
@@ -64,6 +68,7 @@ public class NativeParseActivity extends AppCompatActivity implements View.OnCli
         switch (view.getId()) {
             // 将json格式的字符串{}转换为Java对象
             case R.id.btn_json_obj:
+                jsonToObjByNative();
                 break;
             // 将json格式的字符串[]转换为Java对象的List
             case R.id.btn_arr_list:
@@ -75,5 +80,29 @@ public class NativeParseActivity extends AppCompatActivity implements View.OnCli
             case R.id.btn_special:
                 break;
         }
+    }
+
+    private void jsonToObjByNative() {
+        // [1]获取或创建JSON数据
+        String json = "{\n" +
+                "\t\"id\":2, \"name\":\"大虾\", \n" +
+                "\t\"price\":12.3, \n" +
+                "\t\"imagePath\":\"http://192.168.10.165:8080/L05_Server/images/f1.jpg\"\n" +
+                "}";
+        ShopBean shop = null;
+        try {
+            // [2]解析json
+            JSONObject jsonObj = new JSONObject(json);
+            int id = jsonObj.optInt("id");
+            String name = jsonObj.optString("name");
+            double price = jsonObj.optDouble("price");
+            String imagePath = jsonObj.optString("imagePath");
+            shop = new ShopBean(id, name, price, imagePath);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // [3]显示JSON数据
+        mTvOriginal.setText(json);
+        mTvTransformed.setText(shop.toString());
     }
 }
