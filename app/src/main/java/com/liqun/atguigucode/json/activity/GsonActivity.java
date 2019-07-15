2365857f2d0ff8029ee892a1b3d64803b86aa4ae
MoeRookie <MoeRookie@163.com>
2019-07-15 08:29:47 +0800

    将json格式的字符串[]转换为Java对象的List

--- app/src/main/java/com/liqun/atguigucode/json/activity/GsonActivity.java
+++ app/src/main/java/com/liqun/atguigucode/json/activity/GsonActivity.java
@@ -9,8 +9,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.liqun.atguigucode.R;
 import com.liqun.atguigucode.json.bean.ShopInfo;
+
+import java.util.List;
 
 public class GsonActivity extends Activity implements View.OnClickListener {
 
@@ -57,6 +60,7 @@ public class GsonActivity extends Activity implements View.OnClickListener {
                 break;
             // （2）将json格式的字符串[]转换为Java对象的List
             case R.id.btn_arr_list:
+                arrToListByGson();
                 break;
             // （3）将Java对象转换为json字符串{}
             case R.id.btn_obj_json:
@@ -65,6 +69,30 @@ public class GsonActivity extends Activity implements View.OnClickListener {
             case R.id.btn_list_arr:
                 break;
         }
+    }
+
+    private void arrToListByGson() {
+        // [1]获取或创建json
+        String json = "[\n" +
+                "    {\n" +
+                "        \"id\": 1,\n" +
+                "        \"imagePath\": \"http://192.168.10.165:8080/f1.jpg\",\n" +
+                "        \"name\": \"大虾1\",\n" +
+                "        \"price\": 12.3\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 2,\n" +
+                "        \"imagePath\": \"http://192.168.10.165:8080/f2.jpg\",\n" +
+                "        \"name\": \"大虾2\",\n" +
+                "        \"price\": 12.5\n" +
+                "    }\n" +
+                "]";
+        // [2]json解析
+        Gson gson = new Gson();
+        List<ShopInfo> shopList = gson.fromJson(json,new TypeToken<List<ShopInfo>>(){}.getType());
+        // [3]显示json数据
+        mTvOriginal.setText(json);
+        mTvTransformed.setText(shopList.toString());
     }
 
     private void jsonToObjByGson() {
