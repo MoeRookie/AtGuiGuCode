e747a4e030b9afba62d00ac8dc1a825b52443bcb
MoeRookie <MoeRookie@163.com>
2019-08-16 20:56:07 +0800

    使用xUtils3下载大文件

--- app/src/main/java/com/liqun/atguigucode/xutils3/net/XUtils3NetActivity.java
+++ app/src/main/java/com/liqun/atguigucode/xutils3/net/XUtils3NetActivity.java
@@ -4,20 +4,26 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.liqun.atguigucode.R;
 
 import org.xutils.common.Callback;
+import org.xutils.common.task.PriorityExecutor;
 import org.xutils.http.RequestParams;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.io.File;
+
 @ContentView(R.layout.activity_xutils3_net)
 public class XUtils3NetActivity extends Activity {
     private static final String TAG = XUtils3NetActivity.class.getSimpleName();
@@ -31,6 +37,8 @@ public class XUtils3NetActivity extends Activity {
     private Button mBtnUploadFile;
     @ViewInject(R.id.tv_result)
     private TextView mTvResult;
+    @ViewInject(R.id.progress_bar)
+    private ProgressBar mPb;
     public static Intent newIntent(Context packageContext) {
         Intent intent = new Intent(packageContext, XUtils3NetActivity.class);
         return intent;
@@ -49,12 +57,69 @@ public class XUtils3NetActivity extends Activity {
                 reqGetPost();
                 break;
             case R.id.btn_download_file:
-                Toast.makeText(this, "大文件下载", Toast.LENGTH_SHORT).show();
+                downloadFile();
                 break;
             case R.id.btn_upload_file:
                 Toast.makeText(this, "大文件上传", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    private void downloadFile() {
+        RequestParams params = new RequestParams("http://vfx.mtime.cn/Video/2016/09/02/mp4/160902093947207009_480.mp4");
+        /**
+         * 配置下载
+         */
+        // 设置保存路径
+        params.setSaveFilePath(Environment.getExternalStorageDirectory()+"/atguigu/490.mp4");
+        // 设置可以立即取消下载
+        params.setCancelFast(true);
+        // 设置不要自动根据头信息命名
+        params.setAutoRename(false);
+        // 设置自定义线程池(有效的值范围[1, 3], 设置为 3 时, 可能阻塞图片加载.)
+        params.setExecutor(new PriorityExecutor(3, true));
+        x.http().get(params, new Callback.ProgressCallback<File>() {
+            /**
+             * 当下载成功的时候回调这个方法,并把下载到哪个路径回传过来
+             * @param file
+             */
+            @Override
+            public void onSuccess(File file) {
+                Log.e(TAG, "onSuccess: " + file.toString());
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e(TAG, "onError: " + ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                Log.e(TAG, "onCancelled: " + cex.getMessage());
+            }
+
+            @Override
+            public void onFinished() {
+                Log.e(TAG, "onFinished: ");
+            }
+
+            @Override
+            public void onWaiting() {
+                Log.e(TAG, "onWaiting: ");
+            }
+
+            @Override
+            public void onStarted() {
+                Log.e(TAG, "onStarted: ");
+            }
+
+            @Override
+            public void onLoading(long total, long current, boolean isDownloading) {
+                mPb.setMax((int) total);
+                mPb.setProgress((int) current);
+                Log.e(TAG, "onLoading: " + current + "/" + total + " " + isDownloading);
+            }
+        });
     }
 
     private void reqGetPost() {
